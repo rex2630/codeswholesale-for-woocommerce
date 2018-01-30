6cd43f6f7963081c1a4b190d3957287247505b6c
MKlowan <maciek_kl@wp.pl>
2018-01-30 12:08:44 +0100

    readme update

--- includes/checker/wp-configuration-checker.php
+++ includes/checker/wp-configuration-checker.php
@@ -18,10 +18,10 @@ class WP_ConfigurationChecker
         if (is_array($exlodePhpVersion) && count($exlodePhpVersion) > 0) {
             $phpVersion = (int) str_replace('PHP ', '', strtoupper($exlodePhpVersion[0]));
             if ($phpVersion < 7) {
-                throw new \Exception(sprintf("Too low php version (%s) in shell. Require php version 7.0 or higher", $detailsPhpVersion[0]));
+                throw new \Exception(sprintf("PHP (%s) in shell is no longer compatible. You need an upgrade to PHP 7.0 or higher.", $detailsPhpVersion[0]));
             }
         } else {
-            throw new \Exception("We met issue with your PATH to Php. Contact with your administrator");
+            throw new \Exception("PHP has not been declared as global variable. Please contact your server administrator.");
         }
     }
 
@@ -34,7 +34,7 @@ class WP_ConfigurationChecker
         $output = ExecManager::exec(ExecManager::PHP_PATH, 'db-connection-exec.php', false);
 
         if (count($output) > 0) {
-            throw new \Exception("Something went wrong with your Database connection. Check your wp-config.php and change localhost to 127.0.0.1");
+            throw new \Exception("Something is wrong with your database connection. Please check your wp-config.php and change localhost to 127.0.0.1.");
         }
     }
 }
